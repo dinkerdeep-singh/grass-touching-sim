f7a65a2dd34c7bd111e463f38f80e3883d917dfe
Dinkerdeep Singh <dinkerdeep@gmail.com>
2024-08-21 09:54:40 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const rootRouter = require('./routes/index');
 const cors = require('cors');
+const rootRouter = require('./routes/index');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: 'https://grass-touching-sim.vercel.app'
-}));
+app.use(cors());
 
 app.use('/api/v1', rootRouter);
 
-app.listen(3000, console.log("Listening to port 3000..."));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
